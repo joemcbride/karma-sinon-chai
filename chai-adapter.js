4918aba8dec33b4bdfcacb789c65498a861450ad
Joe McBride <joe@uicraftsman.com>
2015-04-08 17:17:00 -0700

    Do not override expect.

--- chai-adapter.js
+++ chai-adapter.js
@@ -2,6 +2,6 @@ var should;
 
 (function(window) {
   window.should = window.chai.should();
-  window.expect = window.chai.expect;
+  //window.expect = window.chai.expect;
   window.assert = window.chai.assert;
 })(window);
